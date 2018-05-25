14196b421f6489c41f5c57664d8ddba0397e28a8
Marios Antonoudiou <marios.antonoudiou@gmail.com>
2018-05-25 19:05:04 +0300

    Add logic to currency card component

---
+++ frontend/src/components/currency-card.js
@@ -0,0 +1,136 @@
+import React from 'react'
+import debounce from 'debounce';
+import {is, ifElse, pipe, map, find, propEq, prop} from 'ramda';
+import {compose, withState, withStateHandlers, withProps} from 'recompose';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
+import Collapse from '@material-ui/core/Collapse';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Delete from '@material-ui/icons/Delete';
+import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown';
+import { Icon } from '@material-ui/core';
+
+const styles = theme => ({
+	largeField: {
+		fontSize: '2.5rem',
+		'&:before': {
+			borderBottomColor: 'white'
+		},
+	},
+	expandButton: {
+		position: 'absolute',
+		top: '10px',
+		right: '10px',
+		zIndex: 99,
+	}
+})
+
+export const LargeInput = withStyles(styles)(
+	props => <TextField
+		InputProps={{
+			classes: {
+				input: props.classes.largeField,
+				underline: props.classes.largeField
+			}
+		}}
+		InputLabelProps={{
+			shrink: true
+		}}
+		{...props}
+	/>
+)
+
+export const CurrencyCard = compose(
+	withStateHandlers(
+		props => ({
+			expanded: false,
+			currency: 'USD',
+			rate: 1,
+			currencies: props.currencies
+		}),
+		{
+			toggleCard: ({expanded}) => () => ({expanded: !expanded}),
+			setRate: ({rate}) => ifElse(is(Number), value => ({rate: value}), () => ({rate})),
+			setCurrency: ({currencies, currency}) => value => {
+				// ({currency: value, rate: props.currencies.find(c => c.name === value).rate}),
+				const findRate = value => pipe(
+					find(propEq('name', value)),
+					prop('rate'),
+				)(currencies)
+
+				console.log(findRate(value))
+
+				return {
+					currency: value,
+					rate: findRate(value)
+				}
+			}
+		}
+	),
+	withProps(props => ({
+		setRate: e => props.setRate(parseFloat(e.target.value) || 0),
+		setCurrency: e => props.setCurrency(e.target.value),
+	})),
+	withStyles(styles)
+)(props => <Card {...props} style={{ position: 'relative' }}>
+	<IconButton
+		className={props.classes.expandButton}
+		onClick={props.toggleCard}
+		>
+		{props.expanded ? (<ExpandLessIcon />) : (<ExpandMoreIcon />)}
+	</IconButton>
+
+	<CardContent>
+		<LargeInput
+			value={(props.amount * props.rate).toFixed(2)}
+			placeholder="0.00"
+			label={props.currency}
+			onChange={props.onChangeAmount}
+			fullWidth />
+		<Collapse in={props.expanded}>
+			<TextField
+				label="Currency"
+				value={props.currency}
+				margin="normal"
+				onChange={props.setCurrency}
+				select
+				fullWidth
+			>
+				{map(option => (
+					<MenuItem key={option.name} value={option.name}>
+						{option.name}
+					</MenuItem>
+				), props.currencies)}
+			</TextField>
+			<TextField
+				label="Rate"
+				value={props.rate}
+				onChange={props.setRate}
+				inputProps={{
+					type: 'number',
+					step: 0.001,
+					min: 0
+				}}
+				margin="normal"
+				fullWidth
+			/>
+		</Collapse>
+	</CardContent>
+
+
+	<CardActions>
+		<Collapse in={props.expanded}>
+			<Button color="secondary"><Delete /> Delete</Button>
+		</Collapse>
+	</CardActions>
+</Card>
+)
--- frontend/src/stories/index.js
+++ frontend/src/stories/index.js
@@ -1,24 +1,39 @@
 import React from 'react';
 
-import {storiesOf} from '@storybook/react';
-import {action} from '@storybook/addon-actions';
-import {linkTo} from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
 
-import {Button, Welcome} from '@storybook/react/demo';
+import { Button, Welcome } from '@storybook/react/demo';
 
-import {LargeInput} from '../components/large-input'
+import { CurrencyCard, LargeInput } from '../components/currency-card'
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Button', module)
-	.add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-	.add('with some emoji', () => (
-		<Button onClick={action('clicked')}>
-			<span role="img" aria-label="so cool">
-				😀 😎 👍 💯
-      </span>
-		</Button>
-	));
-
 storiesOf('LargeInput', module)
-	.add('default', () => <LargeInput />)
+	.add('default', () => <LargeInput
+		label="USD"
+		placeholder="0.00"
+	/>)
+
+
+const currencies = [
+	{
+		name: 'EUR',
+		rate: 1.3
+	},
+	{
+		name: 'USD',
+		rate: 1.0
+	},
+	{
+		name: 'GBP',
+		rate: 1.6
+	},
+]
+storiesOf('CurrencyCard', module)
+	.add('default', () => <CurrencyCard
+							amount={15}
+							currencies={currencies}
+							onChangeAmount={action('onChangeAmount')}
+							/>)
