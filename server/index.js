60f0c45f01b5b76a3d7097b79cc9e0be5dc333db
Marios Antonoudiou <marios.antonoudiou@gmail.com>
2018-07-10 23:22:50 +0300

    Simplify stuff

--- server/index.js
+++ server/index.js
@@ -1,91 +1,44 @@
 const express = require('express')
 const path = require('path')
-const { GraphQLServer } = require('graphql-yoga')
+const {GraphQLServer} = require('graphql-yoga')
+const {typeDefs} = require('./type-defs')
+const conversionRate = require('./resolver')
+const mem = require('mem')
 const fetch = require('node-fetch')
-const monk = require('monk')
-const { map, keys, isNil, isEmpty } = require('ramda')
-const { typeDefs } = require('./type-defs')
 
-const url = process.env.MONGODB_URI
+const fetchRates = mem(async () => fetch(`https://openexchangerates.org/api/latest.json?app_id=${
+	process.env.API_KEY
+}`).then(res => res.json(), {maxAge: 60 * 60 * 1000}))
 
-const db = monk(url)
+const timestamp = mem(() => new Date(), {maxAge: 60 * 60 * 1000})
 
-const fetchRates = async () => {
-	const res = await fetch(
-		`https://openexchangerates.org/api/latest.json?app_id=${
-			process.env.API_KEY
-		}`,
-	)
-	return res.json()
-}
-
-const saveRates = async () => {
-	const rates = await fetchRates()
-	const bases = keys(rates.rates)
-
-	const collection = db.get('rates')
-
-	bases.forEach(base => {
-		const rate = keys(rates.rates).map(key => ({
-			abbreviation: key,
-			rate: rates.rates[key] / rates.rates[base],
-		}))
-
-		const conversionRate = {
-			base,
-			rates: rate,
-			timestamp: rates.timestamp,
-		}
-
-		collection.findOneAndUpdate(
-			{ base: conversionRate.base },
-			{ $currentDate: { lastModified: true }, $set: conversionRate },
-			{ upsert: true, returnNewDocument: true },
-		)
-	})
-}
-
-const buildRates = (rates, currencies) => {
-	if (!isNil(currencies) && !isEmpty(currencies)) {
-		return currencies.map(currency =>
-			rates.find(rate => rate.abbreviation === currency),
-		)
-	}
-	return rates
-}
-
-const getFromRatesFromDb = base => {
-	const collection = db.get('rates')
-	return collection.findOne({ base })
-}
-
-const resolvers = {
-	Query: {
-		conversionRate: async (_, { base, currencies }) => {
-			const rates = await getFromRatesFromDb(base)
-
-			const euro = map(r => r / rates.rates['EUR'])(rates.rates)
-
-			return {
-				timestamp: rates.timestamp,
-				base: rates.base,
-				rates: buildRates(rates.rates, currencies),
-			}
+const start = async () => {
+	const resolvers = {
+		Query: {
+			conversionRate
 		},
-	},
+	}
+
+	const context = req => ({
+		...req,
+		fetchRates,
+		timestamp,
+	})
+
+	const server = new GraphQLServer({typeDefs, resolvers, context})
+
+	server.express.use(express.static(path.join(__dirname, '../build')))
+
+	server.start(
+		{
+			endpoint: '/graphql',
+			port: process.env.PORT || 4000,
+		},
+		({port}) => {
+			console.log(`Server is listening at port ${port}`)
+		},
+	)
 }
-const server = new GraphQLServer({ typeDefs, resolvers })
 
-saveRates()
+start()
 
-server.express.use(express.static(path.join(__dirname, '../build')))
-
-server.start(
-	{
-		endpoint: '/graphql',
-		port: process.env.PORT || 4000,
-	},
-	({ port }) => {
-		console.log(`Server is listening at port ${port}`)
-	},
-)
---
+++ server/resolver.js
@@ -0,0 +1,39 @@
+const {tap, map, pipeP, } = require('ramda')
+
+const toSingleObject = async (r,a,ctx,i) => ({r,a,ctx,i})
+
+const getRates = async ({r, a, ctx, i}) => {
+	const {fetchRates} = ctx;
+
+	const rates = await fetchRates()
+
+	return {r, a, ctx: {...ctx, rates}, i}
+}
+
+const calculateRates = async (props) => {
+	const {r,a,ctx,i} = props
+	const {rates} = ctx;
+	const {base, currencies} = a
+
+	const baseRate = rates.rates[base]  // Refactor this
+	const calculate = map(currency => ({
+		abbreviation: currency,
+		rate: baseRate / rates.rates[currency]
+	}))
+
+	return {r, a, ctx: {...ctx, rates: calculate(currencies)}, i}
+}
+
+const resolver = async ({a, ctx}) => ({
+	base: a.base,
+	timestamp: ctx.timestamp(),
+	rates: ctx.rates
+})
+
+module.exports = pipeP(
+	toSingleObject,
+	getRates,
+	calculateRates,
+	resolver,
+	tap(console.log),
+)
